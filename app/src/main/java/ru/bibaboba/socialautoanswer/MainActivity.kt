8037da6be49a950378871229b8d63cff59302ad9
AldrFo <mister.fo1996@gmail.com>
2022-08-17 00:20:30 +0300

    Добавил модуль ответчика, начал описывать навигацию

--- app/src/main/java/ru/bibaboba/socialautoanswer/MainActivity.kt
+++ app/src/main/java/ru/bibaboba/socialautoanswer/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.bibaboba.socialautoanswer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import ru.bibaboba.socialautoanswer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navController : NavController by lazy { findNavController(R.id.navFragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding.mainBottomNavigation.setupWithNavController(navController)
     }
+
 }
